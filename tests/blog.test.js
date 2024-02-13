de4b73fe058704598c5f490893bd92806e60e5b3
Javier Suárez <javiersuarsan@gmail.com>
2024-02-13 17:47:40 -0500

    [feature] part4.4

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -6,3 +6,9 @@ test('dummy returns one', () => {
   const result = blogUtils.dummy(blogs)
   expect(result).toBe(1)
 })
+test('total of likes', () => {
+  const blogs = [{likes: 1}, {likes: 4}]
+
+  const result = blogUtils.getTotalOfLikes(blogs)
+  expect(result).toBe(5)
+})
--- utils/blog.utils.js
+++ utils/blog.utils.js
@@ -1,5 +1,12 @@
 const dummy = (blogs) => {
     return blogs.length
 }
+
+const getTotalOfLikes = (blogs) => {
+    return blogs.reduce((acc, curr) => acc += curr.likes, 0)
+}
   
-module.exports = { dummy }
+module.exports = { 
+    dummy, 
+    getTotalOfLikes 
+}
